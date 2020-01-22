dfa2a84dd3d043653b3148eb5392675db146ec50
SuperHarmony910 <safinzaman@outlook.com>
2020-01-22 19:32:07 +1100

    Update Chatbot1.py

--- Chatbot1.py
+++ Chatbot1.py
@@ -1,2 +1,7 @@
+my_age = 11
+half_my_age = my_age / 2
 my_name = "Safin"
-print("Hello and welcome, " + my_name + "!")
+greeting = "Hi! How are you? My name is "
+greeting_with_name = greeting + my_name + ". "
+greeting_with_name_and_age = greeting_with_name + "I am " + my_age + " years old."
+print(greeting_with_name_and_age)
