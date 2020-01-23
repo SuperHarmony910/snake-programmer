53430c09c88c4392ae27f6aab16251bb0c259f0d
SuperHarmony910 <safinzaman@outlook.com>
2020-01-23 14:00:32 +1100

    Update Chatbot1.py

--- Chatbot1.py
+++ Chatbot1.py
@@ -1,7 +1,10 @@
-my_age = 11
-half_my_age = my_age / 2
-my_name = "Safin"
-greeting = "Hi! How are you? My name is "
-greeting_with_name = greeting + my_name + ". "
-greeting_with_name_and_age = greeting_with_name + "I am " + my_age + " years old."
-print(greeting_with_name_and_age)
+def greeting():
+ my_age = 11
+ half_my_age = my_age / 2
+ my_name = "Safin"
+ greeting = "Hi! How are you? My name is "
+ greeting_with_name = greeting + my_name + ". "
+ ask_name_question = "What's yours?"
+ print(greeting_with_name +  ask_name_question)
+
+greeting()
