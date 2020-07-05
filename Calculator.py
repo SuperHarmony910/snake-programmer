f1d63d29de639bf9fc1a0172551c94fcfb87f3f6
SuperHarmony910 <safinzaman@outlook.com>
2020-07-05 22:00:11 +1000

    I might've done it with a teensy bit o' help!
    I think its a calculator. Finally, I can beat what is know as a monstrous threat to humanity; mathematics! (Maths is actually great when utilised to your advantage!)

---
+++ Calculator.py
@@ -0,0 +1,31 @@
+def calculator_interface_start():
+
+    # Take input from user
+    calc_query = input('''
+Do you want to calculate?
+Please type Y for YES or N for NO.
+''')
+
+    # If user has no intent to run the program and presses 'N'
+    calc_deactivate = quit()
+
+    # If user types Y, run the calculate() function
+    if calc_query == 'Y':
+        calculate()
+
+    # If user types N, say good-bye to the user and end the program
+    elif calc_deactivate == 'N':
+        print('See you later.')
+
+    # If user types another key, run the function again
+    else:
+        calculator_interface_start()
+
+def calculate():
+    number_1 = int(input('Enter your first number: '))
+    number_2 = int(input('Enter your second number: '))
+    
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+
+  
