b8fdac267d91acd58833c79330478f11d6d0aff7
SuperHarmony910 <safinzaman@outlook.com>
2020-07-10 16:21:20 +1000

    Update mathLearner.py

--- mathLearner.py
+++ mathLearner.py
@@ -4,7 +4,7 @@ firstInput = input()
 q1ans = 4*3
 if firstInput == str(q1ans):
     score = score + 1
-    print('Correct! Your score so far is ' + str(score) +' out of 2.')
+    print('Correct! Your score is ' + str(score) +' out of 3.')
 else:
     print('Incorrect! The correct answer was ' + str(q1ans) + '! Your answer was ' + firstInput + '!')
 
@@ -13,6 +13,17 @@ secondInput = input()
 q2ans = 6*3
 if secondInput == str(q2ans):
     score = score + 1
-    print('Correct! Your score so far is ' + str(score) +' out of 2.')
+    print('Correct! Your score is ' + str(score) +' out of 3.')
 else:
     print('Incorrect! The correct answer was ' + str(q2ans) + '! Your answer was ' + secondInput + '!')
+
+print('If y = 4, what is 9y?')
+thirdInput = input()
+y = 4
+q3ans = 9*y
+if thirdInput == str(q3ans):
+    score = score + 1
+    print('Correct! Your total score is ' + str(score) +' out of 3.')
+else:
+    print('Incorrect! The correct answer was ' + str(q3ans) + '! Your answer was ' + thirdInput + '!')
+    print("Your total score is " + str(score) + " out of 3!")
