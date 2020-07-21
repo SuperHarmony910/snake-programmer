7382dde009f6c596f682c44f26ac03854cc20008
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-07-21 18:07:13 +1000

    Security reasons

--- Chatbot1.py
+++ Chatbot1.py
@@ -1,8 +1,8 @@
 def greeting():
-  my_age = 11
+  my_age = 99
   # This signifies my age and, as a game, half of my age. I discontinued the "my_age" integer being used
   half_my_age = my_age / 2
-  my_name = "Safin"
+  my_name = "insert"
   greeting = "Hi! How are you? My name is "
   greeting_with_name = greeting + my_name + ". "
   ask_name_question = "What's yours?"
