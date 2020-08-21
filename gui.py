3211b94853261fce84ee35e391110e88fb6084b7
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-21 23:20:57 +1000

    not now...

--- gui.py
+++ gui.py
@@ -1,12 +1,14 @@
 # Credits to https://datatofish.com/entry-box-tkinter/ & https://realpython.com/python-gui-tkinter/
 
 import tkinter as tk
-master = Tk()
 window = tk.Tk()
-entry = tk.Entry(root)
-name = entry.get
+entry = tk.Entry 
+result = entry.get
+name.config(text=result)
+entry.delete(0,END)
 
-greeting_str = tk.Label(text="Hello " + name + "!")
+greet_me = tk.Label(text="Hello " + name + "!")
+
 def greeting():
  greeting_str.pack()
  window.mainloop()
