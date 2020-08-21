56ba7b2b8e9f4d20e8279a1dc0ef3d33609b63be
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-21 22:52:40 +1000

    modificationinginginging...

--- gui.py
+++ gui.py
@@ -1,3 +1,14 @@
+# Credits to https://datatofish.com/entry-box-tkinter/ & https://realpython.com/python-gui-tkinter/
+
 import tkinter as tk
 window = tk.Tk()
-greeting = tk.Label(text="Hello, Saripoo!")
+entry = tk.Entry 
+name = entry.get
+
+greeting = tk.Label(text="Hello " + name + "!")
+def greeting():
+ greeting.pack()
+window.mainloop()
+
+button1 = tk.Button(text="Click me once you've typed your name! ", command=greeting)
+
