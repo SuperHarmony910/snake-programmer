4aa7a655b2840d44c5a505850edf38f08a6aa6a0
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-22 20:35:50 +1000

    An issue log!
    Started an issue log at the top of the page.

--- gui.py
+++ gui.py
@@ -1,20 +1,21 @@
 # Credits to https://datatofish.com/entry-box-tkinter/ & https://realpython.com/python-gui-tkinter/
+# Current issues: entry.get dysfunctional, button in wrong pos!
 
-import tkinter as tk * from tkinter.ttk import *
+import tkinter as tk
 window = tk.Tk()
 entry = tk.Entry 
 result = entry.get
 name = "null"
-name = result
-
+name == result
+window.title("Greetings!") 
 
 greet_me = tk.Label(window, text="Hello " + name + "!")
 
 def greeting():
- greet_me.pack()
+ greet_me.place(relx = 0.5, rely = 0.5)
 
 button1 = tk.Button(window, text="Click me once you've typed your name! ", command=greeting)
-button1.pack
+button1.place(height=100, width=100)
 
-master.geometry("200x200") 
+window.geometry("200x200") 
 window.mainloop()
