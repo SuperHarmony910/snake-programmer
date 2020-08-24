4c279161ef4216126a2d60e248ac5159aa8b3b10
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-24 21:41:39 +1000

    Modified gui.py
    Signed-off-by: SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>

--- gui.py
+++ gui.py
@@ -5,21 +5,21 @@ import tkinter as tk
 
 master = tk.Tk()
 
+master.title("Greetings!")
 canvas1 = tk.Canvas(master, width = 400, height = 300, relief = 'raised')
 canvas1.pack()
 
 entry1 = tk.Entry (master) 
-canvas1.create_window(200, 25, window=entry1)
+canvas1.create_window(200, 145, window=entry1)
 
 def greeting ():  
     name = entry1.get()
-    
     greet_me = tk.Label(master, text = float(name))
     greet_me.config(font=('helvetica', 10))
 
-    canvas1.create_window(200, 230, window=greet_me)
+canvas1.create_window(200, 230, window=greet_me)
     
-button1 = tk.Button(text='Greetings!', command=greeting)
+button1 = tk.Button(text='Greetings!', command=greeting, bg='red', fg='white')
 canvas1.create_window(200, 180, window=button1)
 
 master.mainloop()
