73e4230a5ecfb79499be774f6eb47b86d7124160
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-24 20:32:38 +1000

    My first digitally verified commit! (I hope...)

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@
 import tkinter as tk
 master = tk.Tk()
 entry = tk.Entry
+canvas1.create_window(200, 140, window=entry)
 entry.delete
 result = entry.get
 name = "null"
@@ -15,6 +16,7 @@ entry.pack
 greet_me = tk.Label(window, text="Hello " + name + "!")
 
 def greeting():
+    name = entry.get
  greet_me.place(relx = 0.5, rely = 0.8)
 
 button1 = tk.Button(window, text="Click me once you've typed your name! ", command=greeting)
