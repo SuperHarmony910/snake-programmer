d045d70c270862e078b8acc96e6e13930acdf6d0
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-08-25 17:44:51 +1000

    Modified gui.py

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 # Credits to Data to Fish: https://datatofish.com/entry-box-tkinter/, RealPython: https://realpython.com/python-gui-tkinter/, https://realpython.com/run-python-scripts
-# Current issues: entry.get dysfunctional, def greeting dysfunctional!
+# Current issues: entry.get dysfunctional!
 
 import tkinter as tk
 
@@ -10,7 +10,6 @@ canvas1 = tk.Canvas(master, width = 400, height = 300)
 canvas1.pack()
 
 canvas2 = tk.Canvas(master, width = 400, height = 300)
-canvas2.pack()
 
 entry1 = tk.Entry (master) 
 canvas1.create_window(200, 145, window=entry1)
@@ -18,14 +17,12 @@ canvas1.create_window(200, 145, window=entry1)
 name = entry1.get()
 
 greet_me = tk.Label(master, text = "Hello " + name + "!")
-
 greet_me.config(font=('helvetica', 10))
-def greeting ():  
-    greet_me.place(x = 0.7, y = 0.5)
-
-canvas1.create_window(200, 230, window=greet_me)
+    
+def greeting ():
+    canvas1.create_window(200, 230, window=greet_me)
     
 button1 = tk.Button(text='Greetings!', command=greeting, bg='red', fg='white')
-canvas2.create_window(200, 120, window=button1)
+canvas1.create_window(200, 120, window=button1)
 
 master.mainloop()
