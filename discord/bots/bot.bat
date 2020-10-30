5e24d387f93f8f95f9ad0aaa3b7d2b68ebb1459f
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2020-10-30 15:56:21 +1100

    Adding a banned usrs command! Q: Why did I do this. A: I got banned from a Discord server and am trying to hack my way back in.

---
+++ discord/bots/bot.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Safin\projects\GitHub\snake-programmer\discord\bots
+python bot.py
+pause
--- discord/bots/bot.py
+++ discord/bots/bot.py
@@ -1,6 +1,9 @@
 # bot.py
 import os
+import random
 
+
+from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 
@@ -8,37 +11,47 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = discord.Client();
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} is currently active on Discord!')
+    print(f'{client.user} has connected to Discord!');
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return 0;
+        if message.author == client.user:
+           return;
 
-    if 'hello' or 'hi' in message.content.lower():
-        await message.channel.send('👋 Hello!')
+        if message.content.startswith('hello' or 'hi'):
+            await message.channel.send('Hello 👋 ');
 
 @client.event
 async def on_message(message):
-        if 'poop' in message.content.lower():
+        if message.author == client.user:
+           return
+
+        if message.content.startswith('poop'):
             await message.channel.send('💩');
 
         if '!link' in message.content.lower():
             await message.channel.send('https://github.com/SuperHarmony910 Shameless plug!');
 
-@client.event
-async def on_message(message):
+        if message.content.startswith('!members'):
+            for guild in client.guilds:
+                for member in guild.members:
+                    await message.channel.send(member)
 
-    if message.content.startswith('!members'):
+        if message.content.startswith('!name'):
+           for guild in client.guilds:
+                for name in guild.name:
+                    await message.channel.send(name)
 
-        for guild in client.guilds:
+        if message.content.startswith('!banned_users'):
+           for guild in client.guilds:
+                for banned_users in ctx.guild.bans():
+                    await message.channel.send(banned_users)
 
-            for member in guild.members:
-                await message.channel.send(member)
-           
+
+
 client.run(TOKEN)
